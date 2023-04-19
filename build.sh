b0f3d1832db53ba10bf764727a9bbefbb7ed4f89
TiborGalko <tibor.galko@gmail.com>
2023-04-19 21:42:04 +0200

    Opraveny cuckoo servis

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 
 echo "Instalujem Holmes"
 go install
+
+echo "Building Holmes-Gateway"
 cd Holmes-Gateway/
 go build .
 
@@ -21,8 +23,8 @@ go build .
 echo "Adresar na uploady Holmes-Frontend/push_to_holmes/uploads"
 mkdir ../Holmes-Frontend/push_to_holmes/uploads
 
-#echo "Building Holmes-Totem"
-#cd ../Holmes-Totem/
-#sbt assembly
+echo "Building Holmes-Totem"
+cd ../Holmes-Totem/
+sbt assembly
 
 echo "Instalacia skoncila, run.sh spustite sluzby."
